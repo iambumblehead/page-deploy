752cdbc5326461a103685ce3bd2f0a57d43e6b6b
chris <chris@bumblehead.com>
2017-08-06 23:22:39 -0700

    support parsing of text beside unicode symbols in markdown files

--- src/deploy_html.js
+++ src/deploy_html.js
@@ -1,5 +1,5 @@
 // Filename: deploy_html.js  
-// Timestamp: 2017.08.06-00:55:15 (last modified)
+// Timestamp: 2017.08.06-23:22:29 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>  
 
 
@@ -15,25 +15,6 @@ module.exports = (o => {
 
     return [content, excerpt];
   };
-
-  /*
-  // ⌚ ✑ ★
-  o.extracttitle = content => {
-    const match = String(content).match(/<[^>]*>★(.*)<.[^\/]\/>/ugi),
-          title = match && match[0].slice(0, -1); // remove ellipsis
-
-    if (/2017.06.11-20:59:00/gi.test(content)) {
-    //if (/★/ugi.test(content)) {
-      console.log('megukana', content, match);
-    }
-
-    //if (excerpt) {
-    //  content = content.replace(match[0], excerpt);
-    //}
-
-    return [content, title];
-  };
-   */
 
   return o;
   
