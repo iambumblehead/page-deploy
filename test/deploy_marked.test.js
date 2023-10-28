38a34a2972483461950b17aefcc0c38d9598f9ea
chris <chris@bumblehead.com>
2023-10-28 13:00:35 -0700

    convert deploy_marked.test.js

--- test/deploy_marked.test.js
+++ test/deploy_marked.test.js
@@ -1,16 +1,16 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import timezone_mock from 'timezone-mock';
-import deploy_marked from '../src/deploy_marked.js';
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import timezone_mock from 'timezone-mock'
+import deploy_marked from '../src/deploy_marked.js'
 
-timezone_mock.register('US/Pacific');
+timezone_mock.register('US/Pacific')
 
 test("default should return marked content", () => {
   assert.strictEqual(
     deploy_marked('**hey now**'),
     '<p><strong>hey now</strong></p>\n'
-  );
-});
+  )
+})
 
 test("default should return marked content, w/ highlighted code blocks", () => {
   const MDStringCode = `
@@ -22,7 +22,7 @@ function log () { console.log('yes!') }
   /* eslint-disable max-len */
   const HTMLStringCode = (`
 <p>a code block</p>
-<pre><code class="hljs language-javascript"><span class="hljs-keyword">function</span> <span class="hljs-title function_">log</span> () { <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(<span class="hljs-string">&#x27;yes!&#x27;</span>) }
+<pre><code class="hljs language-javascript"><span class="hljs-keyword">function</span> <span class="hljs-title function_">log</span> () { <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(<span class="hljs-string">&#x27yes!&#x27</span>) }
 </code></pre>
 `).slice(1, -1)
   /* eslint-enable max-len */
@@ -30,7 +30,7 @@ function log () { console.log('yes!') }
   assert.strictEqual(
     HTMLStringCode,
     deploy_marked(MDStringCode))
-});
+})
 
 test("extractsymbols should return symbol mapping", () => {
   const MDStringSymbols = `
@@ -43,11 +43,11 @@ test("extractsymbols should return symbol mapping", () => {
 ★ pyramid
 ==========
 \`✑ bumblehead\`
-_⌚ 2008.09.27-22:45:00_`;
+_⌚ 2008.09.27-22:45:00_`
 
   assert.deepEqual(deploy_marked.extractsymbols(MDStringSymbols)[1], {
-    author : 'bumblehead',
-    timeDate : 1222580700000,
-    title : 'pyramid'
-  });
-});
+    author: 'bumblehead',
+    timeDate: 1222580700000,
+    title: 'pyramid'
+  })
+})
