7091c87d874f4662c3e5ae5d05448ed7153adc83
chris <chris@bumblehead.com>
2023-10-28 12:56:13 -0700

    convert deploy_fileconvert.test.js

--- test/deploy_fileconvert.test.js
+++ test/deploy_fileconvert.test.js
@@ -2,34 +2,34 @@
 // Timestamp: 2017.04.09-01:37:56 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>
 
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import os from 'os';
-import path from 'path';
-import deploy_fileconvert from '../src/deploy_fileconvert.js';
+import os from 'node:os'
+import path from 'node:path'
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import deploy_fileconvert from '../src/deploy_fileconvert.js'
 
-const homedir = os.homedir();
+const homedir = os.homedir()
 
 // in ./inputs/name
 // out $HOME/software/pocket-markdown-converter/ \
 //   getStarted/convert/page-object/forms/sign-up/inputs/name/baseLocale.json
 test("getRefPathFilename should ", () => {
-  const baseLangMDPath = path.join(homedir, 'test/baseLang.md');
+  const baseLangMDPath = path.join(homedir, 'test/baseLang.md')
 
   assert.strictEqual(
     deploy_fileconvert.getRefPathFilename(baseLangMDPath, './inputs/name'),
     path.join(homedir, 'test/inputs/name/baseLang.md')
-  );
-});
+  )
+})
 
 // in ./inputs/name
 // out $HOME/Software/pocket-markdown-converter/ \
 //   getStarted/convert/page-object/forms/sign-up/inputs/name/baseLocale.json
 test("getRefPath should ", () => {
-  const baseLangMDPath = path.join(homedir, 'test/baseLang.md');
+  const baseLangMDPath = path.join(homedir, 'test/baseLang.md')
 
   assert.strictEqual(
     deploy_fileconvert.getRefPath(baseLangMDPath, './inputs/name'),
     path.join(homedir, 'test/inputs/name')
-  );
-});
+  )
+})
