446a794837a745c18deb7c4535805abd3f4cacef
chris <chris@bumblehead.com>
2023-10-29 00:51:55 -0700

    resolve lint. expand linting to all test files

--- test/deploy_imgprocess.test.js
+++ test/deploy_imgprocess.test.js
@@ -5,11 +5,10 @@ import assert from 'node:assert/strict'
 import deploy_imgprocess from '../src/deploy_imgprocess.js'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-const imgpath = `${__dirname}`
 
 test("getisofilenamearr should return iso filenames", async () => {
   const str = 'mock/support/img/saint-christopher.png#pd.fit:1000'
-  const filename = `${imgpath}mock/langLocale.spec`
+  const filename = `${__dirname}mock/langLocale.spec`
   const article = {}
   const res = await util.promisify(deploy_imgprocess.processembeddedimgref)({
     metaurl: import.meta.url,
