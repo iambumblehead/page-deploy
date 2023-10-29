c4b181c9435b22cbca355dc854565401caca30d5
chris <chris@bumblehead.com>
2023-10-29 03:19:40 -0700

    convert opts file to newer esm format

--- src/deploy_opts.js
+++ src/deploy_opts.js
@@ -4,44 +4,35 @@
 
 import castas from 'castas'
 
-export default (o => {
+const defaultopts = {
+  inputDir: './convert/',
+  outputDir: './converted/',
+  publicPath: 'domain.com/converted',
+  supportDir: '',
 
-  const defaultopts = {
-    inputDir: './convert/',
-    outputDir: './converted/',
-    publicPath: 'domain.com/converted',
-    supportDir: '',
+  supportedLangArr: false,
+  supportedLocaleArr: false,
 
-    supportedLangArr: false,
-    supportedLocaleArr: false,
+  // a cache of objects constructed here. often referenced 
+  // many times, but constructed once only using cache    
+  patterncache: {},
+  articlescache: {}
+}
 
-    // a cache of objects constructed here. often referenced 
-    // many times, but constructed once only using cache    
-    patterncache: {},
-    articlescache: {}
-  }
+const getasboolorarr = opt => /true|false/i.test(opt)
+  ? castas.bool(opt)
+  : castas.arr(opt)
 
-  o = spec =>
-    o.getNew(spec)
+export default spec => {
+  const opts = Object.create(defaultopts)
 
-  o.getasboolorarr = opt => /true|false/i.test(opt)
-    ? castas.bool(opt)
-    : castas.arr(opt)
+  opts.inputDir = castas.str(spec.inputDir, './')
+  opts.publicPath = castas.str(spec.publicPath, './spec')
+  opts.outputDir = castas.str(spec.outputDir, './build/spec')
+  opts.supportDir = castas.str(spec.supportDir, '')
+  opts.datetitlesubdirs = castas.arr(spec.datetitlesubdirs, [])
+  opts.supportedLocaleArr = getasboolorarr(spec.supportedLocaleArr)
+  opts.supportedLangArr = getasboolorarr(spec.supportedLangArr)
 
-  o.getNew = spec => {
-    const opts = Object.create(defaultopts)
-
-    opts.inputDir = castas.str(spec.inputDir, './')
-    opts.publicPath = castas.str(spec.publicPath, './spec')
-    opts.outputDir = castas.str(spec.outputDir, './build/spec')
-    opts.supportDir = castas.str(spec.supportDir, '')
-    opts.datetitlesubdirs = castas.arr(spec.datetitlesubdirs, [])
-    opts.supportedLocaleArr = o.getasboolorarr(spec.supportedLocaleArr)
-    opts.supportedLangArr = o.getasboolorarr(spec.supportedLangArr)
-
-    return opts
-  }
-
-  return o
-
-})()
+  return opts
+}
