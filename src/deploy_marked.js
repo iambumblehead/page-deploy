7053aef586dfa0e6c0bacc391d6298c18080f7dc
iambumblehead <cdepauw@gmail.com>
2023-10-29 01:41:36 -0700

    update lint some files (#23)
    Co-authored-by: chris <chris@bumblehead.com>

--- src/deploy_marked.js
+++ src/deploy_marked.js
@@ -2,97 +2,99 @@
 // Timestamp: 2017.08.24-03:00:04 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>
 
-import {Marked} from "marked";
-import {markedHighlight} from "marked-highlight";
-import hljs from 'highlight.js';
+import {Marked} from "marked"
+import {markedHighlight} from "marked-highlight"
+import hljs from 'highlight.js'
 
-import simpletime from 'simpletime';
-import castas from 'castas';
+import simpletime from 'simpletime'
+import castas from 'castas'
 
 const marked = new Marked(
   markedHighlight({
-    langPrefix : 'hljs language-',
+    langPrefix: 'hljs language-',
     highlight (code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, { language }).value;
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
     }
   })
-);
+)
 
-export default (o => {
 
-  o = filestr =>
-    marked.parse(filestr);
+
   
-  o.parsedatestr = (datestr, fmt='yyyy.MM.dd-HH:mm:ss') =>
-    simpletime.extractDateFormatted(datestr, fmt);
+const parsedatestr = (datestr, fmt='yyyy.MM.dd-HH:mm:ss') => (
+  simpletime.extractDateFormatted(datestr, fmt))
 
-  o.parsedatestrtime = datestr =>
-    o.parsedatestr(datestr).getTime();
+const parsedatestrtime = datestr => (
+  parsedatestr(datestr).getTime())
 
-  // return [
-  //   str with symbol matching line removed,
-  //   text found after symbol (if symbol)
-  // ]
-  // 
-  // extractsymboltext('#★ text', '★') => ['', 'text']
-  // extractsymboltext('★ text\n======', '★') => ['', 'text']
-  //
-  //
-  o.extractsymboltext = (str, symbol) => {
-    const symbolre = new RegExp('(.*)?[#_*`]'+symbol+'(.*)', 'ugi'),
-          symbolunderlinere = new RegExp(symbol+'(.*)\n==*', 'ugi'),
-          endmdtagre = /([#_*`]|\n==*)$/,
-          match = (String(str).match(symbolre) ||
-                   String(str).match(symbolunderlinere));
+// return [
+//   str with symbol matching line removed,
+//   text found after symbol (if symbol)
+// ]
+// 
+// extractsymboltext('#★ text', '★') => ['', 'text']
+// extractsymboltext('★ text\n======', '★') => ['', 'text']
+//
+//
+const extractsymboltext = (str, symbol) => {
+  const symbolre = new RegExp('(.*)?[#_*`]'+symbol+'(.*)', 'ugi')
+  const symbolunderlinere = new RegExp(symbol+'(.*)\n==*', 'ugi')
+  const endmdtagre = /([#_*`]|\n==*)$/
+  const match = (String(str).match(symbolre) ||
+                 String(str).match(symbolunderlinere))
 
-    return (match && match[0])
-      ? [ str.replace(match[0], ''),
-        match[0].split(symbol)[1].trim().replace(endmdtagre, '') ]
-      : [ str ];
-  };
+  return (match && match[0])
+    ? [ str.replace(match[0], ''),
+      match[0].split(symbol)[1].trim().replace(endmdtagre, '') ]
+    : [ str ]
+}
 
-  o.extractsymbols = (str, obj={}, text) => 
-    [ [ '★', 'title' ],
-      [ '✑', 'author' ],
-      [ '☆', 'excerptnohtml' ],
-      [ '⌚', 'timeDate', o.parsedatestrtime ]
-    ].reduce(([ str, obj ], [ sym, propname, filter ]) => {
-      [ str, text ] = o.extractsymboltext(str, sym);
+const extractsymbols = (str, obj={}, text) => (
+  [ [ '★', 'title' ],
+    [ '✑', 'author' ],
+    [ '☆', 'excerptnohtml' ],
+    [ '⌚', 'timeDate', parsedatestrtime ]
+  ].reduce(([ str, obj ], [ sym, propname, filter ]) => {
+    [ str, text ] = extractsymboltext(str, sym)
 
-      if (text) {
-        if (filter) {
-          text = filter(text);
-        }
-
-        obj[propname] = text;
+    if (text) {
+      if (filter) {
+        text = filter(text)
       }
 
-      return [ str, obj ];
-      
-    }, [ str, obj ]);
-
-  o.extractmetadata = (str, metadata={}) => {
-    let metaValRe = /\[meta:(.*)\]: <> \((.*)\)/gi;
-
-    if (typeof str === 'string') {
-      str.replace(metaValRe, (match, m1, m2) => {
-        if (/date$/i.test(m1)) {
-          metadata[m1] = o.parsedatestrtime(m2);
-        } else if (/arr$/i.test(m1)) {
-          metadata[m1] = m2.split(/,/);
-        } else if (/^is/.test(m1) &&
-                   /true|false/g.test(m2)) {
-          metadata[m1] = castas.bool(m2);
-        } else {
-          metadata[m1] = m2;
-        }
-      });
+      obj[propname] = text
     }
+
+    return [ str, obj ]
     
-    return [ str, metadata ];
-  };
+  }, [ str, obj ]))
 
-  return o;
+const extractmetadata = (str, metadata={}) => {
+  let metaValRe = /\[meta:(.*)\]: <> \((.*)\)/gi
 
-})({});
+  if (typeof str === 'string') {
+    str.replace(metaValRe, (match, m1, m2) => {
+      if (/date$/i.test(m1)) {
+        metadata[m1] = parsedatestrtime(m2)
+      } else if (/arr$/i.test(m1)) {
+        metadata[m1] = m2.split(/,/)
+      } else if (/^is/.test(m1) &&
+                 /true|false/g.test(m2)) {
+        metadata[m1] = castas.bool(m2)
+      } else {
+        metadata[m1] = m2
+      }
+    })
+  }
+  
+  return [ str, metadata ]
+}
+
+export default Object.assign(marked.parse, {
+  parsedatestr,
+  parsedatestrtime,
+  extractsymboltext,
+  extractsymbols,
+  extractmetadata
+})
